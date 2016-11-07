d7fcbbf218664b167ca092947633f3bc07afe224
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2016-11-07 08:39:45 -1000

    firez beatz nanea

---
+++ yolo.js
@@ -0,0 +1,25 @@
+hero = "yolo"
+gender = "undecided"
+age = "666"
+weapon = "hair"
+pronoun = null
+
+if (gender === undecided){
+pronoun = "gang";
+}
+if (gender === attack helicopter){
+  pronoun = "@@@@@@@@@@";
+}
+else pronoun = "uwot";
+
+if (age < 18){
+  console.log("its just the beginning");
+}
+if (age > 21 && age < 40){
+  console.log("has seen few battles");
+}
+if (age > 40 ) 
+  console.log ("has lived through many epic battles");
+
+
+
