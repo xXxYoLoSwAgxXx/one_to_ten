c884d028916caef0238fea3bcb63cfa8cf4f6283
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2016-11-09 08:12:06 -1000

    finished

--- yolo.js
+++ yolo.js
@@ -23,3 +23,58 @@ if (age > 40 )
 
 
 
+console.log(pronoun + " " + hero + " is our lovable hero, who " + " with his trusty " + weapon ".");
+
+var count ;
+var animal = "zak";
+function animalHouse(x, y){
+
+ count = x + y;
+ console.log(count);
+}
+animalHouse(6, 9);
+
+
+
+if (count >= 2){
+ console.log("We have too many " + animal + "s");}
+
+else{
+ console.log("Welcome to the club");
+}
+
+var hero = "batman"
+function avengersAssemble(avenger){
+
+
+  if (avenger = "batman"){
+ console.log(avenger + " why am i here");}
+  if (hero = "jesus"){
+ console.log(avenger + " in god we trust");}
+  if (hero = "hulk")
+console.log(avenger + " ahhhhhhhhhhhh");}
+  else{
+ console.log("Sorry, go away")}
+}
+avengersAssemble(hero);
+
+var lights = true;
+
+
+if (lights = true){
+ console.log("And then there was light") }
+else{
+ console.log("Lights out")
+
+
+}
+var carType = “tesla”;
+var color = “black”;
+var num = 3005;
+
+if (carType = tesla && color = black){
+ console.log("We have your " + color + " " + carType + ".");
+}
+else{
+ console.log("We don't have any black Teslas in stock.")
+}
